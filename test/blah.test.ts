a92e43aad9deb4d242e5a0a641ad2df573c09636
Emre Kanatli <kanatliemre@gmail.com>
2021-05-13 15:43:59 -0400

    fix test

--- test/blah.test.ts
+++ test/blah.test.ts
@@ -1,8 +1,12 @@
 import MnemonicWallet from '@/Wallet/MnemonicWallet';
 
+const MNEMONIC =
+    'chimney noodle canyon tunnel sample stuff scan symbol sight club net own arrive cause suffer purity manage squirrel boost diesel bring cement father slide';
 describe('blah', () => {
     it('works', () => {
-        const wallet = MnemonicWallet.create();
-        expect(wallet.getAddressX()).toEqual('');
+        const wallet = MnemonicWallet.fromMnemonic(MNEMONIC);
+        expect(wallet.getAddressX()).toEqual('X-avax19v8flm9qt2gv2tctztjjerlgs4k3vgjsfw8udh');
+        expect(wallet.getAddressP()).toEqual('P-avax19v8flm9qt2gv2tctztjjerlgs4k3vgjsfw8udh');
+        expect(wallet.getAddressC()).toEqual('0x6a23c16777a3a194b2773df90feb8753a8e619ee');
     });
 });
