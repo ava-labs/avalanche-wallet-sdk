190487594181ab3861a22624b6907e90278be466
Emre Kanatli <kanatliemre@gmail.com>
2021-06-28 18:16:23 -0400

    add index.ts to utils Move toLocaleString definition to utils

--- src/index.ts
+++ src/index.ts
@@ -8,39 +8,10 @@ import LedgerWallet from '@/Wallet/LedgerWallet';
 import PublicMnemonicWallet from '@/Wallet/PublicMnemonicWallet';
 
 import * as NetworkConstants from '@/Network/constants';
-import * as Utils from '@/utils/utils';
+import * as Utils from '@/utils';
 import Keystore from '@/Keystore/keystore';
 import { BN } from 'avalanche';
 import Big from 'big.js';
-
-declare module 'big.js' {
-    interface Big {
-        toLocaleString(toFixed?: number): string;
-    }
-}
-
-Big.prototype.toLocaleString = function (toFixed: number = 9) {
-    let fixedStr = this.toFixed(toFixed);
-    let split = fixedStr.split('.');
-    let wholeStr = parseInt(split[0]).toLocaleString('en-US');
-
-    if (split.length === 1) {
-        return wholeStr;
-    } else {
-        let remainderStr = split[1];
-
-        // remove trailing 0s
-        let lastChar = remainderStr.charAt(remainderStr.length - 1);
-        while (lastChar === '0') {
-            remainderStr = remainderStr.substring(0, remainderStr.length - 1);
-            lastChar = remainderStr.charAt(remainderStr.length - 1);
-        }
-
-        let trimmed = remainderStr.substring(0, toFixed);
-        if (!trimmed) return wholeStr;
-        return `${wholeStr}.${trimmed}`;
-    }
-};
 
 export {
     MnemonicWallet,
