6daaec4bdee5b230539ee4008d93441a3b4714ed
Josh Hesketh <josh.hesketh@gmail.com>
2021-06-30 15:38:25 +0100

    add config for local node connected to mainnet and added tests

--- test/helpers/network_helper.test.ts
+++ test/helpers/network_helper.test.ts
@@ -1,19 +1,68 @@
 import { rpcUrlFromConfig, wsUrlFromConfigEVM, wsUrlFromConfigX } from '@/helpers/network_helper';
-import { MainnetConfig } from '@/Network/constants';
+import { MainnetLocalConfig, LocalnetConfig, MainnetConfig, TestnetConfig } from '@/Network/constants';
 
 describe('Network helper methods', () => {
-    it('can return ws url for X chain', () => {
+    // Mainnet
+    it('can return ws url for X chain for mainnet', () => {
         let url = wsUrlFromConfigX(MainnetConfig);
         expect(url).toEqual('wss://api.avax.network:443/ext/bc/X/events');
     });
 
-    it('can return ws url for EVM', () => {
+    it('can return ws url for EVM for mainnet', () => {
         let url = wsUrlFromConfigEVM(MainnetConfig);
         expect(url).toEqual('wss://api.avax.network:443/ext/bc/C/ws');
     });
 
-    it('can return rpc url for EVM', () => {
+    it('can return rpc url for EVM for mainnet', () => {
         let url = rpcUrlFromConfig(MainnetConfig);
         expect(url).toEqual('https://api.avax.network:443/ext/bc/C/rpc');
+    });
+
+    // Testnet
+    it('can return ws url for X chain for testnet', () => {
+        let url = wsUrlFromConfigX(TestnetConfig);
+        expect(url).toEqual('wss://api.avax-test.network:443/ext/bc/X/events');
+    });
+
+    it('can return ws url for EVM for testnet', () => {
+        let url = wsUrlFromConfigEVM(TestnetConfig);
+        expect(url).toEqual('wss://api.avax-test.network:443/ext/bc/C/ws');
+    });
+
+    it('can return rpc url for EVM for testnet', () => {
+        let url = rpcUrlFromConfig(TestnetConfig);
+        expect(url).toEqual('https://api.avax-test.network:443/ext/bc/C/rpc');
+    });
+
+    // Localnet
+    it('can return ws url for X chain for localnet', () => {
+        let url = wsUrlFromConfigX(LocalnetConfig);
+        expect(url).toEqual('ws://localhost:9650/ext/bc/X/events');
+    });
+
+    it('can return ws url for EVM for localnet', () => {
+        let url = wsUrlFromConfigEVM(LocalnetConfig);
+        expect(url).toEqual('ws://localhost:9650/ext/bc/C/ws');
+    });
+
+    it('can return rpc url for EVM for localnet', () => {
+        let url = rpcUrlFromConfig(LocalnetConfig);
+        expect(url).toEqual('http://localhost:9650/ext/bc/C/rpc');
+    });
+
+    // Local Mainnet
+    it('can return ws url for X chain for mainnet local', () => {
+        let url = wsUrlFromConfigX(MainnetLocalConfig);
+        expect(url).toEqual('ws://localhost:9650/ext/bc/X/events');
+    });
+
+    it('can return ws url for EVM for mainnet local', () => {
+        let url = wsUrlFromConfigEVM(MainnetLocalConfig);
+        expect(url).toEqual('ws://localhost:9650/ext/bc/C/ws');
+    });
+
+    it('can return rpc url for EVM for mainnet local', () => {
+        let url = rpcUrlFromConfig(MainnetLocalConfig);
+        expect(url).toEqual('http://localhost:9650/ext/bc/C/rpc');
     });
 });
