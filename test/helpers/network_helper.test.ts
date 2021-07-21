7d493eef298c4a5ff00a042a905d345174729673
dfenstermaker <dannyfenstermaker@gmail.com>
2021-07-21 14:08:20 -0600

    update for test

--- test/helpers/network_helper.test.ts
+++ test/helpers/network_helper.test.ts
@@ -1,4 +1,4 @@
-import { rpcUrlFromConfig, wsUrlFromConfigEVM, wsUrlFromConfigX } from '@/helpers/network_helper';
+import { wsUrlFromConfigEVM, wsUrlFromConfigX } from '@/helpers/network_helper';
 import { MainnetConfig } from '@/Network/constants';
 
 describe('Network helper methods', () => {
@@ -10,10 +10,5 @@ describe('Network helper methods', () => {
     it('can return ws url for EVM', () => {
         let url = wsUrlFromConfigEVM(MainnetConfig);
         expect(url).toEqual('wss://api.avax.network:443/ext/bc/C/ws');
-    });
-
-    it('can return rpc url for EVM', () => {
-        let url = rpcUrlFromConfig(MainnetConfig);
-        expect(url).toEqual('https://api.avax.network:443/ext/bc/C/rpc');
     });
 });
