0a3623ceb9fdd40ff1aef8cc495419b69206079b
dfenstermaker <dannyfenstermaker@gmail.com>
2021-07-21 10:12:47 -0600

    create env checks

--- src/Network/index.ts
+++ src/Network/index.ts
@@ -3,20 +3,37 @@ import { MainnetConfig } from '@/Network/constants';
 import { activeNetwork, setRpcNetwork, getEvmChainID } from '@/Network/network';
 import WebsocketProvider from '@/Network/providers/WebsocketProvider';
 import { bustErc20Cache } from '@/Asset/Erc20';
+import { NetworkConstants } from '..';
 
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export function setNetwork(conf: NetworkConfig) {
     setRpcNetwork(conf);
     bustErc20Cache();
 }
 
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+export function isFujiNetwork() {
+    return activeNetwork === NetworkConstants.TestnetConfig;
+}
+
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+export function isMainnetNetwork() {
+    return activeNetwork === NetworkConstants.MainnetConfig;
+}
+
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+export function isLocalNetwork() {
+    return activeNetwork === NetworkConstants.LocalnetConfig;
+}
+
 // Default connection is Mainnet
 setNetwork(MainnetConfig);
 
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export function getAvaxAssetID() {
     return activeNetwork.avaxID;
 }
 
 export { WebsocketProvider, getEvmChainID };
 
-export { activeNetwork } from '@/Network/network';
 export { NetworkConfig } from './types';
