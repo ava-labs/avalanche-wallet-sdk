944d618465196ebf40767eb2b9db22251d90f4ee
Emre Kanatli <kanatliemre@gmail.com>
2021-08-02 23:02:30 -0400

    Add methods to connect to a network config asynchronously with credential check and connectivity check

--- src/helpers/network_helper.ts
+++ src/helpers/network_helper.ts
@@ -1,4 +1,6 @@
 import { NetworkConfig } from '@/Network/types';
+import axios from 'axios';
+import { Avalanche } from 'avalanche';
 
 export function wsUrlFromConfigX(config: NetworkConfig): string {
     let protocol = config.apiProtocol === 'http' ? 'ws' : 'wss';
@@ -8,4 +10,65 @@ export function wsUrlFromConfigX(config: NetworkConfig): string {
 export function wsUrlFromConfigEVM(config: NetworkConfig): string {
     let protocol = config.apiProtocol === 'http' ? 'ws' : 'wss';
     return `${protocol}://${config.apiIp}:${config.apiPort}/ext/bc/C/ws`;
+}
+
+/**
+ * Given the base url of an Avalanche API, requests the Network ID
+ * @param url The base url for the Avalanche API
+ */
+export async function getNetworkIdFromURL(url: string): Promise<number> {
+    // TODO: Not be the best to assume /ext/info but Avalanchejs complicates things
+    let res = await axios.post(url + '/ext/info', {
+        jsonrpc: '2.0',
+        id: 1,
+        method: 'info.getNetworkID',
+    });
+    return parseInt(res.data.result.networkID);
+}
+
+export function createAvalancheProvider(config: NetworkConfig) {
+    return new Avalanche(config.apiIp, config.apiPort, config.apiProtocol, config.networkID);
+}
+/**
+ * Given a network configuration returns an Axios instance connected to the explorer
+ */
+export function createExplorerApi(networkConfig: NetworkConfig) {
+    if (!networkConfig.explorerURL) {
+        throw new Error('Network configuration does not specify an explorer API.');
+    }
+
+    return axios.create({
+        baseURL: networkConfig.explorerURL,
+        withCredentials: false,
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
+}
+
+/**
+ * Checks if the given network accepts credentials.
+ * This must be true to use cookies.
+ */
+export async function canUseCredentials(config: NetworkConfig): Promise<boolean> {
+    let provider = createAvalancheProvider(config);
+    provider.setRequestConfig('withCredentials', true);
+
+    let infoApi = provider.Info();
+
+    // Make a dummy request with credentials
+    try {
+        await infoApi.getNetworkID();
+        return true;
+    } catch (e) {}
+
+    provider.setRequestConfig('withCredentials', false);
+
+    try {
+        await infoApi.getNetworkID();
+    } catch (e) {
+        throw new Error('Unable to connect.');
+    }
+
+    return false;
 }
