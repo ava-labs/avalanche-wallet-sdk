3a1b45909c15e7910e349b581a42ade6b2e5437f
Emre Kanatli <kanatliemre@gmail.com>
2021-12-14 00:06:47 -0500

    remove unnecessary export

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -3,4 +3,3 @@ export * as TxHelper from './tx_helper';
 export * as UtxoHelper from './utxo_helper';
 export * as NetworkHelper from './network_helper';
 export * as GasHelper from './gas_helper';
-export * from '../UniversalTx/UniversalNode';
