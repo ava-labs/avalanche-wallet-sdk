2930b77d050030809fd58258fff86ac2369e1470
Connor Chevli <connorchev@gmail.com>
2021-12-30 19:13:16 +0100

    feat: add support for token prices in different currencies (#44)
    feat: add support for token prices in different currencies

--- src/utils/price_utils.ts
+++ src/utils/price_utils.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const COINGECKO_URL = 'https://api.coingecko.com/api/v3/simple/price?ids=avalanche-2&vs_currencies=usd';
+const getCoinGeckoURL = (currentCurrency: string) =>
+    `https://api.coingecko.com/api/v3/simple/price?ids=avalanche-2&vs_currencies=${currentCurrency}`;
 
 /**
  * Fetches the current AVAX price using Coin Gecko.
@@ -8,9 +9,9 @@ const COINGECKO_URL = 'https://api.coingecko.com/api/v3/simple/price?ids=avalanc
  * You might get rate limited if you use this function frequently.
  *
  * @return
- * Current USD price of 1 AVAX
+ * Current price of 1 AVAX vs a currency (default USD)
  */
-export async function getAvaxPrice(): Promise<number> {
-    const res = await axios.get(COINGECKO_URL);
-    return res.data['avalanche-2'].usd;
+export async function getAvaxPrice(currentCurrency = 'USD'): Promise<number> {
+    const res = await axios.get(getCoinGeckoURL(currentCurrency));
+    return res.data['avalanche-2'][currentCurrency.toLowerCase()];
 }
