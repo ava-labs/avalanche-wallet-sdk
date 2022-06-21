523f16dae8b8bcb7112ff41f7874fbc86f67d226
Emre Kanatli <kanatliemre@gmail.com>
2022-06-21 12:22:00 -0500

    feat: support C to P export imports in history export

--- src/History/importExportParser.ts
+++ src/History/importExportParser.ts
@@ -2,21 +2,23 @@ import { iHistoryImportExport } from '@/History/types';
 import { parseMemo } from '@/History/history_helpers';
 import { idToChainAlias } from '@/Network/helpers/aliasFromNetworkID';
 import { xChain } from '@/Network/network';
-import { bnToAvaxX } from '@/utils';
+import { bnToAvaxX, strip0x } from '@/utils';
 import { getOutputsOfChain, getOutputTotals, getOwnedOutputs } from '@/Explorer/ortelius/utxoUtils';
 import { findDestinationChain, findSourceChain, OrteliusAvalancheTx } from '@/Explorer';
+import { BN } from 'avalanche';
 
-export function getImportSummary(tx: OrteliusAvalancheTx, addresses: string[]): iHistoryImportExport {
+export function getImportSummary(tx: OrteliusAvalancheTx, addresses: string[], evmAddr: string): iHistoryImportExport {
     let sourceChain = findSourceChain(tx);
     let chainAliasFrom = idToChainAlias(sourceChain);
     let chainAliasTo = idToChainAlias(tx.chainID);
 
+    const normalizedEVMAddr = strip0x(evmAddr.toLowerCase());
     let outs = tx.outputs || [];
-    let myOuts = getOwnedOutputs(outs, addresses);
+    let myOuts = getOwnedOutputs(outs, [...addresses, normalizedEVMAddr]);
     let amtOut = getOutputTotals(myOuts);
 
     let time = new Date(tx.timestamp);
-    let fee = xChain.getTxFee();
+    let fee = new BN(tx.txFee);
 
     let res: iHistoryImportExport = {
         id: tx.id,
