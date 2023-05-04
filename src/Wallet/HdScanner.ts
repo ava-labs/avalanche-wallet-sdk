2a81fd9ac3534a8a48cf1a0f861d44dae8293871
Emre Kanatli <emre@avalabs.org>
2023-05-04 15:24:29 +0100

    test: fix test to use new methods, use legacy SSL for testing

--- src/Wallet/HdScanner.ts
+++ src/Wallet/HdScanner.ts
@@ -12,7 +12,7 @@ import {
     SCAN_RANGE,
     SCAN_SIZE,
 } from './constants';
-import { getAddressChains, listChainsForAddresses } from '@/Explorer';
+import { listChainsForAddresses } from '@/Explorer';
 import { NO_NETWORK } from '@/errors';
 import { bintools } from '@/common';
 import { sleep } from '@/utils';
@@ -214,7 +214,6 @@ export class HdScanner {
     // explorer API.
     private async findAvailableIndexExplorer(startIndex = 0): Promise<number> {
         let addrs = await this.getAddressesInRange(startIndex, startIndex + HD_SCAN_LOOK_UP_WINDOW);
-        // let addrChains = await getAddressChains(addrs);
         const addrChainsGlacier = await listChainsForAddresses(addrs, activeNetwork.networkID);
         const seenAddrs = addrChainsGlacier.map((addrData) => addrData.address);
 
